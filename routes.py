e5494ef8665c8a958ca441749359c188cf4f4c7a
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2022-02-06 20:16:00 +0200

    Rekisteröityminen, kirjautuminen ja uloskirjautuminen toteutettu

---
+++ routes.py
@@ -0,0 +1,45 @@
+from app import app
+from flask import render_template, request, redirect
+import users
+
+@app.route("/")
+def index():
+    if users.user_id() == 0:
+        return redirect("/login")
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if users.user_id() != 0:
+        return redirect("/")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if users.user_id() != 0:
+        return redirect("/")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
